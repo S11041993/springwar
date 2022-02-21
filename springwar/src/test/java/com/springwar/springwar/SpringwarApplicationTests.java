2ed4e29fc65cfe9dda489b1c31a47ba487d7f5cf
unknown <soumenadhikari440@gmail.com>
2022-02-21 15:31:16 +0530

    First Commit

---
+++ springwar/src/test/java/com/springwar/springwar/SpringwarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwar.springwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
